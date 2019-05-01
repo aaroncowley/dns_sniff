649833f255c1dffae7170e6c0529e6c759a77dea
Aaron <acow777@gmail.com>
2019-05-01 00:14:26 -0600

    added a menu of local interfaces for convenience

--- sniffer.cc
+++ sniffer.cc
@@ -1,12 +1,32 @@
 #include <tins/tins.h>
 #include <iostream>
 #include <vector>
+#include <set>
 #include <string>
+#include <sys/types.h>
+#include <ifaddrs.h>
 
 using namespace std;
 using namespace Tins;
 
-std::vector<std::string> domains = {"shodan", "google"};
+//global vector for dns filtering
+const static std::vector<std::string> domains = {"shodan", "google"};
+
+std::set<std::string> int_names()
+{
+    ifaddrs *ifap;
+    getifaddrs(&ifap); 
+
+    std::set<std::string> interfaces = {};
+
+    while (ifap->ifa_next != NULL) {
+        interfaces.insert(ifap->ifa_name);
+        ifap = ifap->ifa_next;
+    }
+
+    return interfaces;
+}
+
 
 bool callback(const PDU& pdu)
 {
@@ -26,20 +46,53 @@ bool callback(const PDU& pdu)
 }
 
 
-
 int main(int argc, char* argv[])
 {
+
+    /*
     if(argc != 2) {
         std::cout << "Usage: " <<* argv << " <interface>" << std::endl;
-        return 1;
+        return 1; 
     }
-    // Sniff on the provided interface in promiscuos mode
+    */
+    std::set<string> interfaces = int_names();
+    std::vector<string> int_vector;
+
+    std::cout << "known network interfaces:" << std::endl;
+    int i = 0;
+    for (auto &name : interfaces) {
+        int_vector.push_back(name);        
+        std::cout << i << ": " << name << std::endl;
+        i++;
+    }
+    
+    char sel = {0};
+    while (true) {
+        std::string input = "";
+
+        std::cout << "pick an interface: ";
+        std::getline(cin, input);
+        std::cout << std::endl;
+
+        int char_val = input[0] - '0';
+
+        if (input.length() == 1 && char_val <= int_vector.size()-1) {
+            sel = input[0];
+            break;
+        }
+        cout << "invalid input detected, please try again" << endl;
+    }
+
+    int sel_to_int = sel - '0'; 
+    std::string interface = int_vector.at(sel_to_int);
+ 
+    // Sniff on the provided interface in monitor mode
     SnifferConfiguration config;
     config.set_promisc_mode(true);
     // Only capture udp packets sent to port 53
     config.set_filter("udp and dst port 53");
-    Sniffer sniffer(argv[1], config);
+    Sniffer sniffer(interface, config);
     
     // Start the capture
-    sniffer.sniff_loop(callback);
+    sniffer.sniff_loop(callback); 
 }
